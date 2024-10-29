aa04f347b197165998883c1640286c570ffab084
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-28 21:05:30 -0600

    feat: TravelGuideImages module added to the page

--- app/travel-guides/page.tsx
+++ app/travel-guides/page.tsx
@@ -1,8 +1,6 @@
-// import { BlogPreview, FeaturedTrips, HeroVideo, TravelGrid } from "./_modules";
+import { Typography } from "@/app/_components";
+import { TravelGuideImages } from "@/app//_modules";
 
-import { ImageGrid, Typography } from "@/app/_components";
-
-// import { layoutItems } from "./_lib/data";
 const images = [
   {
     src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
@@ -13,7 +11,33 @@ const images = [
     src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
     alt: "Descripción de la imagen 2",
   },
-  // Agrega más imágenes según sea necesario
+  {
+    src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
+    alt: "Descripción de la imagen 1",
+    href: "/link-a-destino-1", // Opcional
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
+    alt: "Descripción de la imagen 2",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
+    alt: "Descripción de la imagen 1",
+    href: "/link-a-destino-1", // Opcional
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
+    alt: "Descripción de la imagen 2",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
+    alt: "Descripción de la imagen 1",
+    href: "/link-a-destino-1", // Opcional
+  },
+  //   {
+  //     src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
+  //     alt: "Descripción de la imagen 2",
+  //   },
 ];
 
 export default function TravelGuide() {
@@ -21,7 +45,11 @@ export default function TravelGuide() {
     <div>
       <main>
         <Typography variant="h1">Galería de Imágenes</Typography>
-        <ImageGrid images={images} />
+        <TravelGuideImages
+          title="Explora Nuestros Destinos Populares"
+          subtitle="Descubre los lugares más increíbles para tu próxima aventura"
+          images={images}
+        />
       </main>
     </div>
   );
