83a753c1a3d897b77dd8d55b307d81f44c3ebebf
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-28 22:21:05 -0600

    feat: images data updated

--- app/travel-guides/page.tsx
+++ app/travel-guides/page.tsx
@@ -1,11 +1,15 @@
-import { Typography } from "@/app/_components";
 import { TravelGuideImages } from "@/app//_modules";
 
 const images = [
   {
     src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
     alt: "Descripción de la imagen 1",
-    href: "/link-a-destino-1", // Opcional
+    // href: "/link-a-destino-1", // Opcional
+  },
+  {
+    src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
+    alt: "Descripción de la imagen 1",
+    // href: "/link-a-destino-1", // Opcional
   },
   {
     src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
@@ -14,7 +18,7 @@ const images = [
   {
     src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
     alt: "Descripción de la imagen 1",
-    href: "/link-a-destino-1", // Opcional
+    // href: "/link-a-destino-1", // Opcional
   },
   {
     src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
@@ -23,7 +27,7 @@ const images = [
   {
     src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
     alt: "Descripción de la imagen 1",
-    href: "/link-a-destino-1", // Opcional
+    // href: "/link-a-destino-1", // Opcional
   },
   {
     src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
@@ -32,7 +36,7 @@ const images = [
   {
     src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
     alt: "Descripción de la imagen 1",
-    href: "/link-a-destino-1", // Opcional
+    // href: "/link-a-destino-1", // Opcional
   },
   //   {
   //     src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
@@ -44,7 +48,6 @@ export default function TravelGuide() {
   return (
     <div>
       <main>
-        <Typography variant="h1">Galería de Imágenes</Typography>
         <TravelGuideImages
           title="Explora Nuestros Destinos Populares"
           subtitle="Descubre los lugares más increíbles para tu próxima aventura"
