0be1fbdf6d50d704c4fed4e466eea71e946e42d7
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-28 20:59:46 -0600

    feat: travel-guides page created

---
+++ app/travel-guides/page.tsx
@@ -0,0 +1,28 @@
+// import { BlogPreview, FeaturedTrips, HeroVideo, TravelGrid } from "./_modules";
+
+import { ImageGrid, Typography } from "@/app/_components";
+
+// import { layoutItems } from "./_lib/data";
+const images = [
+  {
+    src: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8ZHN5Qk9Wd1VzQWd8fGVufDB8fHx8fA%3D%3D",
+    alt: "Descripción de la imagen 1",
+    href: "/link-a-destino-1", // Opcional
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1661877112841-0efa68b18527?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHxkc3lCT1Z3VXNBZ3x8ZW58MHx8fHx8",
+    alt: "Descripción de la imagen 2",
+  },
+  // Agrega más imágenes según sea necesario
+];
+
+export default function TravelGuide() {
+  return (
+    <div>
+      <main>
+        <Typography variant="h1">Galería de Imágenes</Typography>
+        <ImageGrid images={images} />
+      </main>
+    </div>
+  );
+}
