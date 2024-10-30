e9184581bc035a7cfd0c7f06cacc6a35af8f57a1
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-29 22:49:38 -0600

    feat: TravelGuideDescription module created

--- app/_modules/index.ts
+++ app/_modules/index.ts
@@ -2,6 +2,16 @@ import BlogPreview from "./BlogPreview";
 import FeaturedTrips from "./FeaturedTrips";
 import HeroVideo from "./HeroVideo";
 import TravelGrid from "./TravelGrid";
+import TravelGuideDescription from "./TravelGuideDescription";
 import TravelGuideImages from "./TravelGuideImages";
+import TravelGuideMap from "./TravelGuideMap";
 
-export { BlogPreview, FeaturedTrips, HeroVideo, TravelGrid, TravelGuideImages };
+export {
+  BlogPreview,
+  FeaturedTrips,
+  HeroVideo,
+  TravelGrid,
+  TravelGuideDescription,
+  TravelGuideImages,
+  TravelGuideMap,
+};
