b35a33472129984c1eb3bbd54f96f35798e31f8b
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-31 19:08:29 -0600

    feat: Hero module instance updated in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,30 @@
 import { BlogPreview, FeaturedTrips, HeroVideo, TravelGrid } from "./_modules";
+import { SearchCard } from "@/app/_components";
 import { layoutItems } from "./_lib/data";
+
+const slidesData = [
+  {
+    video_url:
+      // "https://cdn.pixabay.com/video/2020/01/25/31569-387675206.mp4",
+      // "https://cdn.pixabay.com/video/2017/08/02/11087-228113789_large.mp4",
+      // "https://cdn.pixabay.com/video/2023/08/12/175657-854054123_large.mp4",
+      "https://cdn.pixabay.com/video/2024/10/18/236893_large.mp4",
+    image: {
+      url: "https://images.unsplash.com/photo-1489493585363-d69421e0edd3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHRyYXZlbHxlbnwwfDB8MHx8fDA%3D",
+      alt: "Travel Image",
+    },
+  },
+];
 
 export default function Home() {
   return (
     <div>
       <main>
-        <HeroVideo
-          title="Epic Travel Destinations"
-          eyebrow_text="Discover the world"
-          subtitle="Experience the adventure"
-          slidesData={[
-            {
-              video_url:
-                // "https://cdn.pixabay.com/video/2020/01/25/31569-387675206.mp4",
-                // "https://cdn.pixabay.com/video/2017/08/02/11087-228113789_large.mp4",
-                // "https://cdn.pixabay.com/video/2023/08/12/175657-854054123_large.mp4",
-                "https://cdn.pixabay.com/video/2024/10/18/236893_large.mp4",
-              image: {
-                url: "https://images.unsplash.com/photo-1489493585363-d69421e0edd3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHRyYXZlbHxlbnwwfDB8MHx8fDA%3D",
-                alt: "Travel Image",
-              },
-            },
-          ]}
-        />
+        <HeroVideo slidesData={slidesData}>
+          <SearchCard />
+        </HeroVideo>
 
-        <FeaturedTrips
-          title="The Hotest Trips"
-          subtitle="Discover the most amazing adventures, collect experiences for life."
-        />
+        <FeaturedTrips />
 
         <TravelGrid items={layoutItems} variant="variant1" />
 
