b319278c5ea2fa354a8ae9b422caf9cf265006a3
Jose Pablo Karam <karamjp7@gmail.com>
2024-11-13 19:29:59 -0600

    feat: use effect added to correctly update the trips data

--- app/_modules/TripGrid/index.tsx
+++ app/_modules/TripGrid/index.tsx
@@ -1,34 +1,33 @@
-// src/components/TripGrid/TripGrid.tsx
 "use client";
-
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import classNames from "classnames/bind";
-import { TripData } from "@/app/_components/TripCard";
-import { SectionHeading, TripCard, TripFilters } from "@/app/_components";
+import { TripCardView } from "@/app/_types";
+import { TripCard, TripFilters } from "@/app/_components";
 import styles from "./TripGrid.module.scss";
 
 const cx = classNames.bind(styles);
 
 interface TripGridProps {
   className?: string;
-  trips: TripData[];
+  trips: TripCardView[];
 }
 
 export const TripGrid: FC<TripGridProps> = ({ trips, className }) => {
-  const [filteredTrips, setFilteredTrips] = useState<TripData[]>(trips);
+  const [filteredTrips, setFilteredTrips] = useState<TripCardView[]>(trips);
+
+  // Actualizar filteredTrips cuando cambian los trips
+  useEffect(() => {
+    setFilteredTrips(trips);
+  }, [trips]);
 
   return (
     <section className={cx("trip-grid", className)}>
-      <SectionHeading
-        title={"Browse Our Collection of Travel Guides"}
-        className={cx("trip-grid__heading")}
-      />
-
       <TripFilters trips={trips} onFiltersChange={setFilteredTrips} />
+
       <div className={cx("trip-grid__container")}>
-        {filteredTrips.map((trip) => (
+        {filteredTrips.map((trip, index) => (
           <div key={trip.id} className={cx("trip-grid__item")}>
-            <TripCard data={trip} />
+            <TripCard data={trip} isPriority={index < 4} />
           </div>
         ))}
       </div>
