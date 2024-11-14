950e5cd51ae5bd4411adf89d28d329876236560a
Jose Pablo Karam <karamjp7@gmail.com>
2024-11-13 19:22:18 -0600

    feat: SearchCard component made server component

--- app/_components/SearchCard/index.tsx
+++ app/_components/SearchCard/index.tsx
@@ -1,28 +1,28 @@
-"use client";
-// SearchCard.tsx
 import { FC } from "react";
 import classNames from "classnames/bind";
-import { Button, SearchTripsForm, Typography } from "@/app/_components";
+import {
+  AnimatedText,
+  Button,
+  SearchForm,
+  Typography,
+} from "@/app/_components";
 import styles from "./SearchCard.module.scss";
 
 const cx = classNames.bind(styles);
 
 export const SearchCard: FC = () => {
-  const handleSubmit = (values: any) => {
-    console.log("Form data:", values);
-    // Handle search logic and redirect to results here
-  };
-
   return (
     <div className={cx("search-card")}>
-      <Typography
-        variant="h1"
-        align="center"
-        color="white"
-        className={cx("search-card__title")}
-      >
-        Discover Your Next Adventure
-      </Typography>
+      <div className={cx("search-card__title-wrapper")}>
+        <Typography
+          variant="h1"
+          align="center"
+          color="white"
+          className={cx("search-card__title")}
+        >
+          <AnimatedText />
+        </Typography>
+      </div>
       <Typography
         variant="h4"
         fontWeight={600}
@@ -30,7 +30,7 @@ export const SearchCard: FC = () => {
         color="white"
         className={cx("search-card__subtitle")}
       >
-        Find personalized travel guides for every type of traveler.
+        Find travel guides for every type of traveler.
       </Typography>
       <Typography
         variant="p1"
@@ -39,21 +39,17 @@ export const SearchCard: FC = () => {
         color="white"
         className={cx("search-card__description")}
       >
-        Whether you prefer exploring through our guide menu or searching for a
-        customized experience, we're here to help you plan the perfect trip.
+        Where would you like to go today?
       </Typography>
 
-      <div className={cx("search-card__trips-form")}>
-        <SearchTripsForm onSubmit={handleSubmit} />
+      <div className={cx("search-card__form")}>
+        <SearchForm />
       </div>
 
       <div className={cx("search-card__cta")}>
-        {/* <Typography variant="p1" fontWeight={600} align="center" color="white">
-          Prefer browsing our guides?
-        </Typography> */}
         <Button
           variant="link-light"
-          href="/"
+          href="/trips"
           target="_self"
           className={cx("search-card__button")}
         >
