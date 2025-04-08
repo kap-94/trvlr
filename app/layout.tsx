d6dc2276d83aefeb36b46a09ca4686874140561e
Jose Pablo Karam <karamjp7@gmail.com>
2025-04-07 18:32:54 -0600

    feat: add stylesheet link to head for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/vbj3fhz.css" />
+      </head>
       <body className={cx(raleway.className, "layout")}>
         <Header />
         <main>{children}</main>
